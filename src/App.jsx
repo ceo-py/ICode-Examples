451f2561ac5122d2cd76828c5dc8ace49ad1c9eb
ceo-py <sfmf@abv.bg>
2023-09-13 20:41:30 +0300

    GUI SEARCH & DISPLAY API CALL GIT

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from "react";
+import {gitGetApi} from "./GitHubGetRequest.jsx";
+import {SolutionsCode} from "./SolutionsCode.jsx";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [searchTerm, setSearchTerm] = useState('');
+    const [output, setOutput] = useState('');
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    const handleSearchSubmit = async () => {
+        console.log(searchTerm);
+        setOutput(await gitGetApi(searchTerm))
+    };
+
+    return (
+        <>
+            <div className="App">
+                <h1>Search App</h1>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                />
+                <button onClick={handleSearchSubmit}>Search</button>
+            </div>
+            <SolutionsCode output={output}/>
+        </>
+    )
 }
+
 
 export default App
