23a850b8c73e06780f45cb4494896c0dda1b7647
ceo-py <sfmf@abv.bg>
2023-11-05 13:14:41 +0200

    fresh start

--- src/utils/formatTitle.jsx
+++
@@ -1,5 +0,0 @@
-export function formatTitle(title) {
-  const withoutUnderscores = title.replace(/_/g, ' ');
-  const titleName = withoutUnderscores.split('.')[0];
-  return titleName.replace(/\b\w/g, char => char.toUpperCase());
-}
