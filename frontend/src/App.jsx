7470eac563f1c0e603a427c890daa92e2eeedec5
ceo-py <sfmf@abv.bg>
2023-11-16 20:31:30 +0200

    GUI snipped button pos & remove css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,24 @@
-import { selectedCourseSignal } from './components/ui/SelectMenu/SelectLanguage/selectMenuSignal';
-import { CodeSnippet } from './components/ui/CodeSnippet/CodeSnippet';
-import { SelectMenu } from './components/ui/SelectMenu/SelectMenu';
-import { SearchInput } from './components/ui/SearchInput/SearchInput';
-import { NavMenu } from './components/ui/NavMenu/NavMenu';
-
+import { selectedCourseSignal } from "./components/ui/SelectMenu/SelectLanguage/selectMenuSignal";
+import { CodeSnippet } from "./components/ui/CodeSnippet/CodeSnippet";
+import { SelectMenu } from "./components/ui/SelectMenu/SelectMenu";
+import { SearchInput } from "./components/ui/SearchInput/SearchInput";
+import { NavMenu } from "./components/ui/NavMenu/NavMenu";
 
 function App() {
-
   return (
     <>
-    <NavMenu menu={selectedCourseSignal}/>
-    <br/>
-    <SelectMenu menu={selectedCourseSignal}/>
-    <br/>
-    {selectedCourseSignal?.value?.selectedModule && <SearchInput/>}
-    <br/>
-    <CodeSnippet/>
+      <NavMenu menu={selectedCourseSignal} />
+      <br />
+      {/* <div className="flex items-center justify-center">
+        <SelectMenu menu={selectedCourseSignal} />
+      <br />
+      {selectedCourseSignal?.value?.selectedModule && <SearchInput />}
+      <br />
+      <CodeSnippet />
+      </div> */}
+      <CodeSnippet />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
