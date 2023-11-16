f9fcb36c0fd74943aa1009985537bc45d91c8055
ceo-py <sfmf@abv.bg>
2023-11-16 11:37:32 +0200

    GUI fix autocomplete in course component

--- frontend/src/components/ui/SelectMenu/SelectCourse/SelectCourse.jsx
+++ frontend/src/components/ui/SelectMenu/SelectCourse/SelectCourse.jsx
@@ -9,7 +9,7 @@ export function SelectCourse({ menu }) {
         onSelectionChange={(x) => {
           menu.value = { ...menu?.value, ...{module: menu?.value?.modules[x]}, ...{selectedCourse: x} };
         }}
-        inputValue = {menu?.value?.selectedCourse? menu.value.selectedCourse: ''}
+        defaultSelectedKey = {menu?.value?.selectedCourse? menu.value.selectedCourse: ''}
         startContent={
           <Avatar
             alt=""
