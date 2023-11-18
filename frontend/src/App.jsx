4f0151abf6ccfb5628353dd1207bbc8622e11f62
ceo-py <sfmf@abv.bg>
2023-11-18 13:24:58 +0200

    GUI card CodeSniped

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,22 @@
 import { selectedCourseSignal } from "./components/ui/SelectMenu/SelectLanguage/selectMenuSignal";
-import { CodeSnippet } from "./components/ui/CodeSnippet/CodeSnippet";
 import { SelectMenu } from "./components/ui/SelectMenu/SelectMenu";
 import { SearchInput } from "./components/ui/SearchInput/SearchInput";
 import { NavMenu } from "./components/ui/NavMenu/NavMenu";
+import { CodeSnippet } from "./components/ui/CodeSnippet/COdeSnippet";
+import { CodeCard } from "./components/ui/CodeCard/CodeCard";
 
 function App() {
   return (
     <>
       <NavMenu menu={selectedCourseSignal} />
-      <br />
-      {/* <div className="flex items-center justify-center">
-        <SelectMenu menu={selectedCourseSignal} />
+      <div class="flex w-full h-auto items-center justify-center overflow-auto mt-[var(--navbar-height)]">
+        {/* <SelectMenu menu={selectedCourseSignal} />
       <br />
       {selectedCourseSignal?.value?.selectedModule && <SearchInput />}
-      <br />
-      <CodeSnippet />
-      </div> */}
-      <CodeSnippet />
+      <br /> */}
+      <CodeCard/>
+      {/* <CodeSnippet /> */}
+      </div>
     </>
   );
 }
