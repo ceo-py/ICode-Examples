19b751800f60d723d90dc423e8f138b579d5e030
ceo-py <sfmf@abv.bg>
2023-11-23 20:58:06 +0200

    GUI modal max char len

--- frontend/src/components/ui/CodeCard/CardButtons/CardReportBtnModal/CardReportBtnModal.jsx
+++ frontend/src/components/ui/CodeCard/CardButtons/CardReportBtnModal/CardReportBtnModal.jsx
@@ -7,8 +7,11 @@ import {
   Button,
   Textarea,
 } from "@nextui-org/react";
+import { signal } from "@preact/signals-react";
 
 export function CardReportBtnModal({ isOpen, onOpenChange }) {
+  const textSize = signal('');
+
   return (
     <>
       <Modal isOpen={isOpen} placement="top-center" onOpenChange={onOpenChange}>
@@ -22,13 +25,16 @@ export function CardReportBtnModal({ isOpen, onOpenChange }) {
                 <Textarea
                   variant="underlined"
                   className="col-span-12 md:col-span-6 mb-6 md:mb-0"
+                  value={textSize.value}
+                  onValueChange={(v) => v.length <= 300 || v.length < text.length? textSize.value = v: ''}
+                  description={`${300 - textSize.value.length} characters remaining`}
                 />
               </ModalBody>
               <ModalFooter>
                 <Button color="danger" variant="light" onPress={onClose}>
                   Close
                 </Button>
-                <Button color="primary" onPress={onClose}>
+                <Button color="primary" onPress={() => {console.log(textSize.value); onClose()}}>
                   Report
                 </Button>
               </ModalFooter>
@@ -39,3 +45,5 @@ export function CardReportBtnModal({ isOpen, onOpenChange }) {
     </>
   );
 }
+
+
