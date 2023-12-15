865e5c96036e4c971a46eb83ea962b3ea88da331
ceo-py <sfmf@abv.bg>
2023-12-15 17:20:20 +0200

    back end on user delete delete everything in db connect with

--- backend/src/GraphQL/Resolvers/Mutation/deleteUser.js
+++ backend/src/GraphQL/Resolvers/Mutation/deleteUser.js
@@ -1,4 +1,7 @@
+const Comments = require("../../../DataBase/Models/comments");
 const Followers = require("../../../DataBase/Models/followers");
+const Likes = require("../../../DataBase/Models/likes");
+const TaskSolution = require("../../../DataBase/Models/taskSolutions");
 const UserDetail = require("../../../DataBase/Models/userDetails");
 const User = require("../../../DataBase/Models/users");
 const jwt = require('jsonwebtoken');
@@ -8,11 +11,11 @@ const deleteUserResolver = {
     Mutation: {
         deleteUser: async (_, { __ }, { req, res }) => {
             const cookieToken = req?.cookies?.token;
-            
+
             if (!cookieToken) {
                 return {
                     message: 'JWT must be provided',
-                    code: 400,
+                    code: 401,
                 };
             }
             try {
@@ -21,6 +24,18 @@ const deleteUserResolver = {
                 await UserDetail.deleteOne({ id });
                 await User.deleteOne({ _id: id });
                 await Followers.deleteOne({ id });
+                await TaskSolution.deleteMany({ id })
+                await Comments.deleteMany({ createdById: id })
+
+                await Followers.updateMany(
+                    { followers: id },
+                    { $pull: { followers: id } }
+                );
+                await Likes.updateMany(
+                    { likes: id },
+                    { $pull: { likes: id } }
+                );
+
                 return {
                     message: 'User Successfully Deleted',
                     code: 200,
