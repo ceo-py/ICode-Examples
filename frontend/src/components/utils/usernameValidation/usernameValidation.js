3250f43fe4c7019a8575703f0be4f2ce80991195
ceo-py <sfmf@abv.bg>
2023-12-18 20:34:15 +0200

    front end username len to 3

--- frontend/src/components/utils/usernameValidation/usernameValidation.js
+++ frontend/src/components/utils/usernameValidation/usernameValidation.js
@@ -1,5 +1,5 @@
 export function usernameValidation(username) {
-    if (!/^[a-zA-Z]{4,10}$/.test(username)) {
-        return 'Create a username that is at least 4-10 characters long can include only letters.';
+    if (!/^[a-zA-Z]{3,10}$/.test(username)) {
+        return 'Create a username that is at least 3-10 characters long can include only letters.';
     }
 }
