c109ecc1b49611d143bf3ae52672ec4f0ce0bea3
ceo-py <sfmf@abv.bg>
2023-12-29 16:33:19 +0200

    backend email setup

---
+++ backend/src/Email/emailServer.js
@@ -0,0 +1,31 @@
+const SibApiV3Sdk = require('@getbrevo/brevo');
+const path = require('path');
+const dotenv = require('dotenv');
+
+const pathToEnvFile = path.resolve(__dirname, '../../../.env');
+dotenv.config({ path: pathToEnvFile });
+
+
+let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+let apiKey = apiInstance.authentications['apiKey'];
+apiKey.apiKey = process.env.EMAIL_API_KEY;
+
+let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail(); 
+
+sendSmtpEmail.subject = "My TEST";
+sendSmtpEmail.htmlContent = "<html><body><h1>This is my first transactional email test</h1></body></html>";
+sendSmtpEmail.sender = {"name":"ICode-Example","email":"icode.example@gmail.com"};
+sendSmtpEmail.to = [{"email":"istrationr@gmail.com","name":"Jane Doe"}];
+// sendSmtpEmail.cc = [{"email":"example2@example2.com","name":"Janice Doe"}];
+// sendSmtpEmail.bcc = [{"name":"John Doe","email":"example@example.com"}];
+// sendSmtpEmail.replyTo = {"email":"replyto@domain.com","name":"John Doe"};
+// sendSmtpEmail.headers = {"Some-Custom-Name":"unique-id-1234"};
+// sendSmtpEmail.params = {"parameter":"My param value","subject":"New Subject"};
+
+apiInstance.sendTransacEmail(sendSmtpEmail).then(function(data) {
+  console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+
+}, function(error) {
+  console.error(error);
+});
