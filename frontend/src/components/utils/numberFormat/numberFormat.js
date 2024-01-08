6eb9a66aa9a1a6182d4e523cead7df7ae7a42b79
ceo-py <sfmf@abv.bg>
2024-01-08 11:54:18 +0200

    front end number format added one decimal

--- frontend/src/components/utils/numberFormat/numberFormat.js
+++ frontend/src/components/utils/numberFormat/numberFormat.js
@@ -2,5 +2,8 @@
 export function numbersFormat(num) {
     const magnitude = Math.min(parseInt((Math.abs(num).toString().length - 1) / 3), 4);
     num /= Math.pow(10, magnitude * 3);
-    return num.toString().split('.')[0] + ' KMBT'[magnitude];
+
+    if (num % 1 !== 0) num = num.toFixed(1);
+
+    return num.toString() + ' KMBT'[magnitude];
 }
