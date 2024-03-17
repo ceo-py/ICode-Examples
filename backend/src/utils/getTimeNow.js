db117299992e5dfc17b6920fb5c8527f7c57fe10
ceo-py <sfmf@abv.bg>
2024-03-17 19:25:09 +0200

    backend added weeks and months in comment time past

--- backend/src/utils/getTimeNow.js
+++ backend/src/utils/getTimeNow.js
@@ -7,9 +7,13 @@ function timeTimeDifference(createTimeDB) {
     const minutes = Math.floor(seconds / 60);
     const hours = Math.floor(minutes / 60);
     const days = Math.floor(hours / 24);
+    const weeks = Math.floor(days / 7);
+    const months = Math.floor(weeks / 4);
     const years = Math.floor(days / 365);
 
     if (years) return `${years} ${years === 1? 'year': 'years'}`
+    if (months) return `${months} ${months === 1? 'month': 'months'}`
+    if (weeks) return `${weeks} ${weeks === 1? 'week': 'weeks'}`
     if (days) return `${days} ${days === 1? 'day': 'days'}`
     if (hours) return `${hours} ${hours === 1? 'hour': 'hours'}`
     if (minutes) return `${minutes} ${minutes === 1? 'minute': 'minutes'}`
